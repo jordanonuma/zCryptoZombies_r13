ea3755850db76cfbd47b7d7a65dbb5a3abebe5e3
jordan <jordanonuma@outlook.com>
2023-03-17 19:43:25 -1000

    Sends front end a notification when zombie has been created.

--- Contract.sol
+++ Contract.sol
@@ -1,6 +1,9 @@
 pragma solidity  >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna)
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -13,6 +16,8 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push() - 1;
+        emit NewZombie(id, _name, _dna);
     } //end function _createZombie()
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
