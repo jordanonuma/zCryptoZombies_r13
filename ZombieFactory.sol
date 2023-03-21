85c2035c8755f781e51e606c4ff55a76a68929ce
jordan <jordanonuma@outlook.com>
2023-03-21 11:12:28 -1000

    Added frame of contract that inherits from ZombieFactory{}.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -35,3 +35,7 @@ contract ZombieFactory {
         _createZombie(_name, randDna);
     } //end function createRandomZombie()
 } //end contract ZombieFactory{}
+
+contract ZombieFeeding is ZombieFactory {
+    
+} //end contract ZombieFeeding{}
