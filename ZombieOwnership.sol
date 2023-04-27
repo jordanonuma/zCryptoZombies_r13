b301f24ddb06a8561128b70ee78638987ddd2232
jordan <jordanonuma@outlook.com>
2023-04-27 10:18:22 -1000

    Added functions for user-zombie stats.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -4,5 +4,19 @@ import "./ZombieAttack.sol";
 import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
-    
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerZombieCount[_owner];
+    } //end function balanceOf()
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return zombieToOwner[_tokenId];
+    } //end function ownerOf()
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+    } //end function transferFrom()
+
+    function approve(address _approved, uint256 _tokenId) external payable {
+ 
+    } //end function approve()
 } //end contract ZombieOwnership{}
