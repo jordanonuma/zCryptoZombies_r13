3c4976159106132972c2021a1fd1acdb9d33f8b9
jordan <jordanonuma@outlook.com>
2023-06-11 20:32:52 -1000

    Initialized struct that contains states of game.

---
+++ Zombie Game Modes/ExampleGame.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.4.25;
+
+import "./ZB/ZBGameMode.sol";
+
+contract ExampleGame is ZBGameMode  {
+
+    function beforeMatchStart(bytes serializedGameState) external {
+        GameState memory gameState;
+        gameState.init(serializedGameState);
+    } //end function beforeMatchStart()
+
+}
