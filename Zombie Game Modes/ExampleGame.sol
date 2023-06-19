eb7b42d03eef231940d2e1891487effa25790e52
jordan <jordanonuma@outlook.com>
2023-06-18 16:39:12 -1000

    Checks if card is playable before adding it to player's deck.

--- Zombie Game Modes/ExampleGame.sol
+++ Zombie Game Modes/ExampleGame.sol
@@ -53,8 +53,15 @@ contract ExampleGame is ZBGameMode  {
     } //end function beforeMatchStart()
 
     for(uint i = 0; i < gameState.playerStates.length; i++) {
-            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
-            uint cardCount = 0;
-    } //end for()
+        CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+        uint cardCount = 0;
+
+        for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+            if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                cardCount++;
+            } //end if()
+        } //end for(j)
+    } //end for(i)
 
 } //end contract ExampleGame{}
