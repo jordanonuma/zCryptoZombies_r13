828c1e54cf42c7f8b0f9f26d15fe004219ef4b0a
Jordan <jordanonuma@outlook.com>
2023-06-21 12:59:37 -1000

    Added contract frame for new zombie game mode.

---
+++ Zombie Game Modes/Munchkin.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.4.25;
+
+import "./ZB/ZBGameMode.sol";
+
+contract ExampleGame is ZBGameMode  {
+
+    function beforeMatchStart(bytes serializedGameState) external {
+        GameState memory gameState;
+        gameState.init(serializedGameState);
+
+        ZBSerializer.SerializedGameStateChanges memory changes;
+        changes.init();
+
+        changes.emit();
+    } //end function beforeMatchStart()
+
+} //end contract ExampleGame{}
