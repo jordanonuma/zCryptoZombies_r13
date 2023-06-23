7bd378bccf5eb6252881b883be611242e2311bdf
jordan <jordanonuma@outlook.com>
2023-06-23 11:08:52 -1000

    Passes playable card to temporary card array newCards[].

--- Zombie Game Modes/Munchkin.sol
+++ Zombie Game Modes/Munchkin.sol
@@ -11,7 +11,25 @@ contract ExampleGame is ZBGameMode  {
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
 
+        for (uint i = 0; i < gameState.playerStates.length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
+
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                } //end if()
+            } //end for(j)
+
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
+        } //end for(i)
+
         changes.emit();
     } //end function beforeMatchStart()
+
+    function isLegalCard(CardInstance card) internal view returns(bool) {
+
+    } //end function isLegalCard()
 
 } //end contract ExampleGame{}
