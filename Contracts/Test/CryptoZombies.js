8a57595aa0435f6d677809891830d169d1016105
jordan <jordanonuma@outlook.com>
2023-07-12 14:07:44 -0700

    Initializes instance of contract.

--- Contracts/Test/CryptoZombies.js
+++ Contracts/Test/CryptoZombies.js
@@ -2,7 +2,8 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
-    
+
     it("should be able to create a new zombie", async () => {
+        const contractInstance = new CryptZombies.new();
     }) //end it()
 }) //end contract()
