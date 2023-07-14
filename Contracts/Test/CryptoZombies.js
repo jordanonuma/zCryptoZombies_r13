bf09d7388b1f78551827c485fa12b8f246c5973f
jordan <jordanonuma@outlook.com>
2023-07-13 21:20:51 -0700

    Checks if function was successfully called.

--- Contracts/Test/CryptoZombies.js
+++ Contracts/Test/CryptoZombies.js
@@ -4,6 +4,9 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
 
     it("should be able to create a new zombie", async () => {
-        const contractInstance = new CryptZombies.new();
+        const contractInstance = new CryptoZombies.new();
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        assert.equal(result.receipt.status, true);
+        assert.equal(result.logs[0].args.name, zombieNames[0]);
     }) //end it()
 }) //end contract()
