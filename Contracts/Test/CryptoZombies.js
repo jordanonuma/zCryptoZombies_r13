68ec1df5e273b8e21388a511b42c875c50c71473
jordan <jordanonuma@outlook.com>
2023-07-15 11:22:53 -1000

    Checks for error if user has more than one zombie.

--- Contracts/Test/CryptoZombies.js
+++ Contracts/Test/CryptoZombies.js
@@ -1,4 +1,6 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
+const utils = require("./helpers/utils");
+const zombieNames = ["Zombie 1", "Zombie 2"];
 
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
@@ -17,6 +19,7 @@ contract("CryptoZombies", (accounts) => {
     }) //end it()
 
     it("should not allow two zombies", async () => {
-
+        await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
     }) //end it()
 }) //end contract()
