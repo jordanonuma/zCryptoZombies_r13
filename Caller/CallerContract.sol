a720c36ec13993c8e8d4d3d21811222599068df6
jordan <jordanonuma@outlook.com>
2023-07-29 15:24:00 -1000

    Added frame of contract pointing to oracle's contract address.

---
+++ Caller/CallerContract.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.5.0;
+contract CallerContract {
+    
+    address private oracleAddress;
+    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+        oracleAddress =_oracleInstanceAddress;
+    } //end function setOracleInstanceAddress();
+} //end contract CallerContract{}
