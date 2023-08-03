75bd226f258e174701d02b8b924bc142a93a60d5
Jordan <jordanonuma@outlook.com>
2023-08-02 18:29:24 -1000

    Added call to solidity contract.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -5,10 +5,19 @@ import "./../Ownable.sol";
 contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    mapping(uint256=>bool) myRequests;
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
+
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress =_oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
         emit newOracleAddressEvent(oracleAddress);
     } //end function setOracleInstanceAddress();
+
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
+    } //end function updateEthPrice()
 } //end contract CallerContract{}
