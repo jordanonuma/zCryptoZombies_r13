13fc3d258c0abd48702e7690f0064fa3bd10a649
jordan <jordanonuma@outlook.com>
2023-08-04 18:10:37 -1000

    Checks for request in queue before saving response to temporary placeholder.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -4,10 +4,12 @@ import "./../Ownable.sol";
 
 contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
+    uint256 private ethPrice;
     address private oracleAddress;
     mapping(uint256=>bool) myRequests;
     event newOracleAddressEvent(address oracleAddress);
     event ReceivedNewRequestIdEvent(uint256 id);
+    event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress =_oracleInstanceAddress;
@@ -20,4 +22,11 @@ contract CallerContract is Ownable {
         myRequests[id] = true;
         emit ReceivedNewRequestIdEvent(id);
     } //end function updateEthPrice()
+
+    function callback(uint256 _ethPrice, uint256 _id) public {
+        require(myRequests[_id], "This request is not in my pending list.");
+        ethPrice = _ethPrice;
+        delete myRequests[_id];
+        emit PriceUpdatedEvent(_ethPrice, _id);
+    } //end function callback()
 } //end contract CallerContract{}
