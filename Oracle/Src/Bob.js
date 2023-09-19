b352343fc75e1f54a3a0443fa6c4707d6de60dda
jordan <jordanonuma@outlook.com>
2023-09-19 11:33:25 -1000

    Declared provider for both chains.

--- Oracle/Src/Bob.js
+++ Oracle/Src/Bob.js
@@ -3,5 +3,6 @@
     const zksync = require('zksync')
     const utils = require('./utils')
   
-    
+    const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+    const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
 })()
