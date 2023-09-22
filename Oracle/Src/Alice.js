11f738354b3ccf6851b83965f372fafb6488eb24
jordan <jordanonuma@outlook.com>
2023-09-22 09:28:16 -1000

    Added function calls to cover basic zksync usage.

--- Oracle/Src/Alice.js
+++ Oracle/Src/Alice.js
@@ -19,6 +19,8 @@
     const aliceZkSyncWallet = await utils.initAccount(aliceRinkebyWallet, zkSyncProvider, zksync)
   
     console.log('Depositing')
-
+    await utils.depositToZkSync(aliceZkSyncWallet, token, amountToDeposit, ethers)
+    await utils.displayZkSyncBalance(aliceZkSyncWallet, ethers)
+    await utils.registerAccount(aliceZkSyncWallet)
   
   })()
