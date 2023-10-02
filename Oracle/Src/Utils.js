68e8709dedf591193624a2de27f23bd8a7657126
jordan <jordanonuma@outlook.com>
2023-10-02 11:37:23 -1000

    Revised token parameters for depositing to zkSync.

--- Oracle/Src/Utils.js
+++ Oracle/Src/Utils.js
@@ -39,11 +39,11 @@ async function registerAccount (wallet) {
     
 } //end function registerAccount()
 
-async function depositToZkSync(zkSyncWallet, token, amountToDeposit, ethers) {
+async function depositToZkSync(zkSyncWallet, token, amountToDeposit, tokenSet) {
     const deposit = await zkSyncWallet.depositToSyncFromEthereum({
       depositTo: zkSyncWallet.address(),
       token: token,
-      amount: ethers.utils.parseEther(amountToDeposit)
+      amount: tokenSet.parseToken(token, amountToDeposit)
     })
     try {
       await deposit.awaitReceipt()
